1bf520147c51ab5063e868521d24b064e7d65a46
Stefan Hoffmann <stho32github@gmail.com>
2024-11-26 18:45:47 +0100

    changed header accept

--- src/web.rs
+++ src/web.rs
@@ -17,7 +17,6 @@ pub fn get_website_content(url: &str) -> (Option<String>, Option<String>) {
         .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
         .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
         .header("Accept-Language", "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7")
-        .header("Accept-Encoding", "gzip, deflate, br")
         .header("Cache-Control", "max-age=0")
         .header("DNT", "1")
         .header("Connection", "keep-alive")
